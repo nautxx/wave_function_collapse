1997d5d98198f930122c60a7d68a14a6db196979
naut <jrbarnachea@gmail.com>
2024-03-27 13:43:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
 from grid import Grid
 from tile import Tile
+
+pygame.init()
+
+# global variables
+width = 600
+height = 600
+display = pygame.display.set_mode((width, height))
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
