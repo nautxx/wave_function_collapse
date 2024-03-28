0e71bd1a6c0ee29a2287e296a510b299a2e1030f
naut <jrbarnachea@gmail.com>
2024-03-28 16:14:22 -0700

    Add rotate() method

--- tile.py
+++ tile.py
@@ -1,4 +1,19 @@
+import pygame
+
 class Tile:
     def __init__(self, img, edges):
         self.img = img
         self.edges = edges
+
+    def rotate(self, num):
+        w, h = self.img.get_size()
+        new_img = pygame.Surface((w, h), pygame.SRCALPHA)
+        new_img.blit(self.img, (0, 0))
+        new_img = pygame.transform.rotate(new_img, -90 * num)
+
+        new_edges = []
+        length = len(self.edges)
+        for i in range(length):
+            new_edges.append(self.edges[(i - num + length) % length])
+
+        return Tile(new_img, new_edges)
